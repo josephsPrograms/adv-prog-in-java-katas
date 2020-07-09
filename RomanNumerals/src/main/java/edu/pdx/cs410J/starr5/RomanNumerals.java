78bbd4f1cc70625fee575ce94b6ee059727516a6
zzxxzz2569 <eddie_huy@yahoo.com.vn>
2020-07-09 10:47:18 +0700

    Added dictionary

--- RomanNumerals/src/main/java/edu/pdx/cs410J/starr5/RomanNumerals.java
+++ RomanNumerals/src/main/java/edu/pdx/cs410J/starr5/RomanNumerals.java
@@ -35,6 +35,39 @@ public class RomanNumerals {
       Map<Integer,String> table = new HashMap<>();
       table.put(1, "I");
       table.put(2, "II");
+      table.put(3,"III");
+      table.put(4,"IV");
+      table.put(5,"V");
+      table.put(6,"VI");
+      table.put(7,"VII");
+      table.put(8,"VIII");
+      table.put(9,"IX");
+      table.put(10,"X");
+      table.put(20,"XX");
+      table.put(30,"XXX");
+      table.put(40,"XL");
+      table.put(50,"L");
+      table.put(60,"LX");
+      table.put(70,"LXX");
+      table.put(80,"LXXX");
+      table.put(90,"XC");
+      table.put(100,"C");
+      table.put(200,"CC");
+      table.put(300,"CCC");
+      table.put(400,"CD");
+      table.put(500,"D");
+      table.put(600,"DC");
+      table.put(700,"DCC");
+      table.put(800,"DCCC");
+      table.put(900,"CM");
+      table.put(1000,"M");
+      table.put(2000,"MM");
+      table.put(3000,"MMM");
+//      if(table.get(Integer.toString(numberRepresentation)))
+//      while(numberRepresentation>0){
+//        int remainder  = numberRepresentation%10;
+//        if(table.get(remainder))
+//      }
       return "0";
     }
   }
